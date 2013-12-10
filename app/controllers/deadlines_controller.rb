eea0e445f3172f46f1b0d8f8cde903e2c574d3f9
oulrich1 <oemulrich@gmail.com>
2013-12-10 12:06:33 -0800

    fixed deadline association issue where all people with deadliens were beings grabbed, now only the members that belong to that seleected deadline are erndered/shown

--- app/controllers/deadlines_controller.rb
+++ app/controllers/deadlines_controller.rb
@@ -11,7 +11,7 @@ class DeadlinesController < ApplicationController
   # GET /deadlines/1.json
   def show
     @deadline = Deadline.find(params[:id])
-    @deadline_members = Member.where("id in(select m_id from member_deadline_groupings)")
+    @deadline_members = Member.where("id in(select m_id from member_deadline_groupings as MPG where MPG.d_id = #{@deadline.id})")
   end
 
   # GET /deadlines/new
